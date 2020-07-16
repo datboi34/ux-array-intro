e373d1829590532e0618795fc861900f18be5c44
datboi34 <edgar87250@gmail.com>
2020-07-16 16:11:09 -0400

    array practice need to keep in mind how to use properties to access objects remember to use dot(.)

--- code.js
+++ code.js
@@ -15,12 +15,12 @@ let sentence = ["The", "dog", "jumped", "over", "the", "fence"];
 // Log the word "jumped" to the console by accessing it in the array.
 
 // Your Code Here
-
+console.log(sentence[2]);
 // Step Two ------------------
 // Log the last word in the array to the console.
 
 // Your Code Here
-
+console.log(sentence[5]);
 /*
 ---------------------------------------------------------------------------
 Exercise Two - Adding up numbers
@@ -34,7 +34,8 @@ Then log the sum to the console. (Hint: It should be 28)
 let numbers = [3, 4, 5, 7, 9];
 
 // Your Code Here
-
+let sum = numbers.reduce((a, b) => a + b);
+console.log(sum);
 /*
 ---------------------------------------------------------------------------
 Exercise Three - Creating an array
@@ -46,10 +47,10 @@ Exercise Three - Creating an array
 // The array should be assigned to a variable named "colors"
 
 // Your Code Here
-
+let colors = ["purple", "red", "blue", "violet", "orange"];
 // Step Two ------------------
 // Log the length of the array to the console (it should print 5)
-
+console.log(colors.length);
 /*
 ---------------------------------------------------------------------------
 Exercise Four - Updating an array
@@ -63,14 +64,16 @@ let names = []; // Don't modify this line.
 // Then log your array to the console.
 
 // Your Code Here
-
+names.push("fred", "steph", "lebron", "kobe", "shaq");
+console.log(names);
 // Step Two ------------------
 // Using the index variable, assign a new name to the given index.
 // Then log the modified array to the console.
 let index = 2;
 
 // Your Code Here
-
+index = ["fresh", "bread"];
+console.log(index);
 /*
 ---------------------------------------------------------------------------
 Exercise Five - Concatenating an array
@@ -86,3 +89,5 @@ let firstArray = ["This", "will", "make"];
 let secondArray = ["a", "combined", "array."];
 
 // Your Code Here.
+let thirdArray = firstArray.concat(secondArray);
+console.log(thirdArray);
